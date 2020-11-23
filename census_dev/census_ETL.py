5551af5086dc3d6714dce5de97b80adbdb38dc7b
Kobol-Coder <lugalot82@hotmail.com>
2020-11-23 10:58:11 -0800

    removing some debug lines from ETL script

--- census_dev/census_ETL.py
+++ census_dev/census_ETL.py
@@ -10,16 +10,16 @@ re_col = re.compile('^B\d.+_\d+[a-zA-Z]?')
 for column in df.columns:
     if re_col.match(column):
         var_list.append(column)
-        print(var_list)
+        # print(var_list)
     else:
         print("FAIL:", column)
-print("VAR:",var_list)
+# print("VAR:",var_list)
 column_list = var_list.copy()
 column_list.append("GEOID")
 new_df = df[column_list]
 print(new_df)
 for var in var_list:
-    print("!!",var)
+    # print("!!",var)
     for i, row in new_df.iterrows():
         print(row['GEOID'],var,row[var],sep='|')
 
