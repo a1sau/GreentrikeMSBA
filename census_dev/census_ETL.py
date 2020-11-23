3d4fa4bba70a520e3ca2ba975fb2e1a0e9313ea4
Kobol-Coder <lugalot82@hotmail.com>
2020-11-23 10:57:19 -0800

    Adding basic census ETL script

---
+++ census_dev/census_ETL.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import re
+
+column_list = []
+var_list = []
+outcsv = {}
+df = pd.read_csv(r'C:\Users\Lugal\OneDrive\Documents\MSBA\Project\pierceCensus2.csv')
+print(df.columns)
+re_col = re.compile('^B\d.+_\d+[a-zA-Z]?')
+for column in df.columns:
+    if re_col.match(column):
+        var_list.append(column)
+        print(var_list)
+    else:
+        print("FAIL:", column)
+print("VAR:",var_list)
+column_list = var_list.copy()
+column_list.append("GEOID")
+new_df = df[column_list]
+print(new_df)
+for var in var_list:
+    print("!!",var)
+    for i, row in new_df.iterrows():
+        print(row['GEOID'],var,row[var],sep='|')
+
+
+##Output = GEOID,variable_ID,value
--- census_dev/census_pull.py
+++ census_dev/census_pull.py
@@ -25,23 +25,23 @@ def plot_shape():
 
 def basic_pull():
     print('product')
-    product=cp.ACS().from_county('Pierce, WA', level='tract', variables=('B01001_003E','B01001_027E','^B19001_'),return_geometry=False)
+    product=cp.ACS().from_county('Pierce, WA', level='tract', variables=('B01001_003','B01001_027','^B19001_'),return_geometry=False)
     # product=cp.ACS().from_county('Pierce, WA', level='tract', return_geometry=False)
     # variables.to_csv("C:/Users/Lugal/OneDrive/Documents/MSBA/Project/pierceCensusVariables.csv")
     print('tacoma')
     # tacoma = product.from_county('Pierce, WA', level='tract', variables=('B01001_003E','B01001_027E'))
     tacoma = product
-    tacoma.to_csv("C:/Users/Lugal/OneDrive/Documents/MSBA/Project/pierceCensus2.csv")
+    tacoma.to_csv("C:/Users/Lugal/OneDrive/Documents/MSBA/Project/pierceCensus3.csv")
     for tract in tacoma:
         print(tract)
         print(tacoma[tract])
-    print(tacoma.head())
-    fig,ax = plt.subplots(figsize = (15,15))
-    tacoma.plot(ax=ax, column='B01001_003E',legend=True)
-    fig.show()
-    tacoma.plot(ax=ax, column='B01001_027E')
-    fig.show()
-    # print(tacoma[0:5][1:3])
+    # print(tacoma.head())
+    # fig,ax = plt.subplots(figsize = (15,15))
+    # tacoma.plot(ax=ax, column='B01001_003E',legend=True)
+    # fig.show()
+    # tacoma.plot(ax=ax, column='B01001_027E')
+    # fig.show()
+    # # print(tacoma[0:5][1:3])
     # # print(product.from_place("Tacoma, WA",'B001001'))
     # # print(cp.ACS.variables())
     return None
@@ -51,7 +51,7 @@ def basic_pull():
 print("start")
 cp.set_sitekey("6050a96e1b4bd539c1813f17d6607d70760fd718",True)
 basic_pull()
-# plot_shape()
+#plot_shape()
 
 # Call to pull male and female kids ages 0-5
 # https://api.census.gov/data/2018/acs/acs5?get=NAME,B01001_003E,B01001_027E&for=block%20group:*&in=state:53%20county:053&key=6050a96e1b4bd539c1813f17d6607d70760fd718
