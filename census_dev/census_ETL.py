71272bd6f7fadfba9968990d421f6ceeb0c39067
Kobol-Coder <lugalot82@hotmail.com>
2021-01-15 19:55:11 -0800

    Handle blank value case

--- census_dev/census_ETL.py
+++ census_dev/census_ETL.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import math
 import sys
 
 def build_import_csv(infile: str, outfile_str: str):
@@ -25,11 +26,13 @@ def build_import_csv(infile: str, outfile_str: str):
     for var in var_list:
         # print("!!",var)
         for i, row in new_df.iterrows():
-            if row[var] == row[var]:
+            if math.isnan(row[var]):   # handle blank values
+                value = ""
+            elif int(row[var]) == row[var]:
                 value = int(row[var])
             else:
                 value = row[var]
-            # print(int(row['GEOID']), var, value, sep='|')
+            #print(int(row['GEOID']), var, value, sep='|')
             print(int(row['GEOID']), var, value, sep='|', file=outfile)
     outfile.close()
     print('Import file created at',outfile_str)
@@ -37,6 +40,6 @@ def build_import_csv(infile: str, outfile_str: str):
 
 
 # build_import_csv(r'C:\Users\Lugal\OneDrive\Documents\MSBA\Project\pierceCensus4.csv',r'C:\Users\Lugal\OneDrive\Documents\MSBA\Project\pierceCensus4OUT.csv')
-build_import_csv(r'C:\Users\Benjamin\Documents\UWTacoma\MSBA\Aplied Project with Greentrike\Cencus_Test\pierceCensus10.csv',
-                 r'C:\Users\Benjamin\Documents\UWTacoma\MSBA\Aplied Project with Greentrike\Cencus_Test\pierceCensus_nonull_1_15_20_OUT.csv')
+build_import_csv(r'C:\Users\Lugal\OneDrive\Documents\MSBA\Project\pierceCensus10.csv',
+                 r'C:\Users\Lugal\OneDrive\Documents\MSBA\Project\pierceCensus_nonull_1_15_20_OUT.csv')
 ##Output = GEOID,variable_ID,value
