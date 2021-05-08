d118cdb3c0486d46c0e6daa3c88534f9220f07d0
Kobol-Coder <lugalot82@hotmail.com>
2021-05-08 10:06:07 -0700

    cleaned up nn_building_script

--- Optimize/NuralNet/nn_building_script.R
+++ Optimize/NuralNet/nn_building_script.R
@@ -21,7 +21,7 @@ get_data <- function(scores_or_new, server, user, password, database, port){
                         PWD      = as.character(password),
                         server = as.character(server),
                         port = port)
-  import.building <- 'SELECT
+  sql.building <- 'SELECT
     bld."CS_ID"
     ,avg(bs."Score") "Average Building Score"
     ,bld."Address_Line"
@@ -33,55 +33,12 @@ get_data <- function(scores_or_new, server, user, password, database, port){
     ,bld."SquareFeet"
     ,round(cast(coalesce(bld."Price" / bld."SquareFeet",NULL) as numeric),0) "$ per sq ft"
      ,bld."Sale_Type"
-     ,bg.bg_geo_id "Block Group ID"
-     ,avg(bgs.score) "Average Block Group Score"
-     ,max(case when dv.sid=\'pop\' then bgd.value Else 0 END) "Population"
-     ,max(case when dv.sid=\'pop_MF_3MS\' then bgd.value Else 0 END) "Population: 3 Miles"
-     ,max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) "Households: 3 Miles"
-    ,max(case when dv.sid=\'M_0_5\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_0_5\' then bgd.value Else 0 END) "Kids under 5"
-     ,round(cast((max(case when dv.sid=\'M_0_5\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_0_5"\' then bgd.value Else 0 END)) /
-        max(case when dv.sid=\'pop\' then bgd.value Else null END) as numeric),3) "Percent Kids under 5"
-    ,max(case when dv.sid=\'M_0_5_3MS\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_0_5_3MS\' then bgd.value Else 0 END) "Kids under 5: 3 Miles"
-     ,round(cast((max(case when dv.sid=\'M_0_5_3MS\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_0_5_3MS\' then bgd.value Else 0 END)) /
-        max(case when dv.sid=\'pop_MF_3MS\' then bgd.value Else null END) as numeric),3) "Percent Kids under 5: 3 Miles"
-    ,max(case when dv.sid=\'M_5_9\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_5_9\' then bgd.value Else 0 END) "Kids 5 to 9"
-     ,round(cast((max(case when dv.sid=\'M_5_9\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_5_9\' then bgd.value Else 0 END)) /
-        max(case when dv.sid=\'pop\' then bgd.value Else null END) as numeric),3) "Percent Kids 5 to 9"
-    ,max(case when dv.sid=\'M_5_9_3MS\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_5_9_3MS\' then bgd.value Else 0 END) "Kids 5 to 9: 3 Miles"
-    ,round(cast((max(case when dv.sid=\'M_5_9_3MS\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_5_9_3MS\' then bgd.value Else 0 END)) /
-         max(case when dv.sid=\'pop_MF_3MS\' then bgd.value Else null END) as numeric),3)  "Percent Kids 5 to 9: 3 Miles"
-    ,max(case when dv.sid=\'avg_age\' then bgd.value Else 0 END) "Average Age"
-    ,round(cast(sum(case when dv.sid in(\'hi_0_10_3MS\',\'hi_10_15_3MS\',\'hi_15_20_3MS\',\'hi_20_25_3MS\',\'hi_25_30_3MS\',\'hi_30_35_3MS\',\'hi_35_40_3MS\') then bgd.value  else 0 END) /
-      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income under 40K: 3 Mile"
-    ,round(cast(sum(case when dv.sid in(\'hi_40_45_3MS\',\'hi_45_50_3MS\') then bgd.value  else 0 END) /
-      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 40K to 50K: 3 Mile"
-    ,round(cast(sum(case when dv.sid in(\'hi_50_60_3MS\') then bgd.value  else 0 END) /
-      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 50K to 60K: 3 Mile"
-    ,round(cast(sum(case when dv.sid in(\'hi_60_75_3MS\') then bgd.value  else 0 END) /
-      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 60K to 75K: 3 Mile"
-    ,round(cast(sum(case when dv.sid in(\'hi_75_100_3MS\') then bgd.value  else 0 END) /
-      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 75K to 100K: 3 Mile"
-    ,round(cast(sum(case when dv.sid in(\'hi_100_125_3MS\') then bgd.value  else 0 END) /
-      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 100K to 125K: 3 Mile"
-    ,round(cast(sum(case when dv.sid in(\'hi_125_150_3MS\') then bgd.value  else 0 END) /
-      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 125K to 150K: 3 Mile"
-    ,round(cast(sum(case when dv.sid in(\'hi_150_200_3MS\') then bgd.value  else 0 END) /
-      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 150K to 200K: 3 Mile"
-    ,round(cast(sum(case when dv.sid in(\'hi_200_999_3MS\') then bgd.value  else 0 END) /
-      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 200K+: 3 Mile"
-from "Building" as bld
-left join "Block_Group" as bg on bg.bg_geo_id = bld.bg_geo_id
-left join "BG_Data" as bgd on bg.bg_geo_id = bgd.bg_geo_id
-inner join "Demo_Var" as dv on dv.full_variable_id=bgd.variable_id
-left join "BG_Score" as bgs on bg.bg_geo_id = bgs.bg_geo_id
-left join "Building_Score" as bs on bld."CS_ID" = bs.cs_id
-group by bld."CS_ID",bld."Address_Line",bld."City",bld."Postal_Code",bld."Property_Type",bld."Price",bld."Year_Built",bld."SquareFeet",bld."Sale_Type",bg.bg_geo_id
-having
-    max(case when dv.sid=\'pop\' then bgd.value Else 0 END) > 0
-    and max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) > 0;'
+    from "Building" as bld
+    left join "Building_Score" as bs on bld."CS_ID" = bs.cs_id
+    where bld."Sale_Lease"=\'Sale\'
+    group by bld."CS_ID",bld."Address_Line",bld."City",bld."Postal_Code",bld."Property_Type",bld."Price",bld."Year_Built",bld."SquareFeet",bld."Sale_Type"'
   
-  building.import <- dbGetQuery(con,import.building)
-  
+  building.import <- dbGetQuery(con,sql.building)
   if(scores_or_new == 'scores'){
     building.scores <- sqldf('SELECT CS_ID, "Average Building Score", Address_Line, City, Postal_Code, Property_Type, Year_Built, Price, SquareFeet, "$ per sq ft", Sale_Type FROM "building.import" WHERE "Average Building Score" IS NOT NULL')
     return(building.scores)
@@ -98,8 +55,7 @@ having
 }
 
 
-clean_building_data <- function(dataframe){
-  build.scores <- dataframe
+clean_building_data <- function(build.scores){
   #build.scores$Score <- as.factor(build.scores$Score)
   #dummy code catigorical variables
   build.scores <- fastDummies::dummy_cols(build.scores, select_columns = "City")
@@ -117,21 +73,38 @@ clean_building_data <- function(dataframe){
                                                      City_Gig_Harbor, City_Lakewood, City_Puyallup, City_Spanaway, City_Tacoma, Property_Type_Flex, 
                                                      Property_Type_Industrial, Property_Type_Office, Property_Type_Retail, Sale_Type_Investment, 
                                                      Sale_Type_Investment_or_Owner_User, Sale_Type_Owner_User, Sale_Type_OwnerUser))
-  
-  non_na <- complete.cases(building.scores[, c("Address_Line", "Price", "SquareFeet", "price_per_sq_ft", "City_Fife", "City_Gig_Harbor", "City_Lakewood", 
+  non_na <- complete.cases(building.scores[, c("Price", "SquareFeet", "price_per_sq_ft", "City_Fife", "City_Gig_Harbor", "City_Lakewood", 
                                                "City_Puyallup", "City_Spanaway", "City_Tacoma", "Property_Type_Flex", "Property_Type_Industrial", 
                                                "Property_Type_Office",         "Property_Type_Retail", "Sale_Type_Investment", 
                                                "Sale_Type_Investment_or_Owner_User", "Sale_Type_Owner_User", "Sale_Type_OwnerUser")])
   out <- building.scores[non_na, ]
   #normalize numeric variables
-  out$Price <- (out$Price - min(out$Price))/(max(out$Price) - min(out$Price))
-  out$SquareFeet <- (out$SquareFeet - min(out$SquareFeet))/(max(out$SquareFeet) - min(out$SquareFeet))
-  out$price_per_sq_ft <- (out$price_per_sq_ft - min(out$price_per_sq_ft))/(max(out$price_per_sq_ft) - min(out$price_per_sq_ft))
-  out$Average_Building_Score <- (out$Average_Building_Score - min(out$Average_Building_Score                                                                        ))/(max(out$Average_Building_Score) - min(out$Average_Building_Score))
+  out$Price <- convert_to_01(out$Price)
+  out$SquareFeet <- convert_to_01(out$SquareFeet)
+  out$price_per_sq_ft <- convert_to_01(out$price_per_sq_ft)
+  out$Average_Building_Score <- convert_to_01(out$Average_Building_Score)
   #remove duplicates
-  out <- out[!duplicated(out[ , "Price"]),]
+  # out <- out[!duplicated(out[ , "Price"]),]
   
   return(out)
+}
+
+#Convert array of numbers to a range from 0 to 1
+convert_to_01<-function(array,min.amt=NULL,max.amt=NULL){
+  if (is.null(min.amt)){
+    min.amt <- min(array)
+  }
+  if (is.null(max.amt)){
+    max.amt<-max(array)
+  }
+  new.range <- (array-min.amt)/(max.amt-min.amt)
+  return(new.range)
+}
+
+#Convert array of numbers from 0-1 range to full range
+convert_from_01<-function(array,min.amt,max.amt){
+  full.range=(array*(max.amt-min.amt))+min.amt
+  return(full.range)
 }
 
 nn.model.score <- function(data){
@@ -149,19 +122,18 @@ nn.model.score <- function(data){
 }
 
 nn.model.new <- function(server, user, password, database, port, data){
-  got_scores_data <- get_data("scores", as.character(server), as.character(user), as.character(password), as.character(database), port)
-  cleaned.scores.data <- clean_building_data(got_scores_data)
+  scores_data <- get_data("scores", server, user, password, database, port)
+  cleaned.scores.data <- clean_building_data(scores_data)
   #insert model
-  set.seed(3)
   build.new.model <- neuralnet(Average_Building_Score ~ Price + SquareFeet + City_Tacoma + City_Puyallup + Property_Type_Industrial + 
                                  Property_Type_Office + Sale_Type_Investment + Sale_Type_Investment_or_Owner_User + Sale_Type_Owner_User, 
                                data = cleaned.scores.data, linear.output = T, hidden = c(5,1), act.fct = "logistic")
   return(build.new.model)
 }
 
-new.predictions <- function(data1, data2){
-  Scores.predict.new <- predict(data1, data2, all.units = FALSE)
-  return(Scores.predict.new)
+new.predictions <- function(model, data2){
+  pred.scores <- convert_from_01(predict(model, newdata=data2, all.units = FALSE),1,5)
+  return(pred.scores)
 }
 
 mainfunction.scores <- function(server, user, password, database, port){
@@ -175,7 +147,7 @@ mainfunction.scores <- function(server, user, password, database, port){
   return(score.model)
 }
 
-mainfunction.scores('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'xxx', 'xxxxxx', 'TEST', 5432)
+# mainfunction.scores('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'xxx', 'xxxxxx', 'TEST', 5432)
 
 mainfunction.new <- function(server, user, password, database, port){
   #get the data
@@ -190,26 +162,34 @@ mainfunction.new <- function(server, user, password, database, port){
   
   #then predict with new
   
-  Scores.predict.new <- new.predictions(model.new,clean.new.data) * 5
+  Scores.predict.new <- convert_from_01(new.predictions(model.new,clean.new.data),1,5)
   return(Scores.predict.new)
 }
 
-mainfunction.new('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'xxx', 'xxxxxx', 'TEST', 5432)
+# mainfunction.new('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'xxx', 'xxxxxx', 'TEST', 5432)
 
 mainfunction.all <- function(server, user, password, database, port){
   #get the data
-  
-  got_all_data <- get_data("all", as.character(server), as.character(user), as.character(password), as.character(database), port)
+  print("Getting Data")
+  building.data <- get_data("all", server, user, password, database, port)
   #clean the data
-  
-  clean.all.data <- clean_building_data(got_all_data)
+  print("cleaning Data")
+  clean.building.data <- clean_building_data(building.data)
   #load in the subfunction
-  
-  model.all <- nn.model.new('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'xxx', 'xxxxxx', 'TEST', 5432)
+  print("Modeling Data")
+  model.all <- nn.model.new(server, user, password, database, port)
   #predict with scores first
-  
-  Scores.predict.all <- new.predictions(model.all, clean.all.data) * 5
-  return(Scores.predict.all)
+  print("Predicting data")
+  Scores.predict.raw <- new.predictions(model.all, clean.building.data)
+  Scores.rounded<-round(Scores.predict.raw)
+  Scores.rounded[Scores.rounded<1] <- 1
+  Scores.rounded[Scores.rounded>5] <- 5
+  df<-as.data.frame(clean.building.data$CS_ID)
+  df$raw_score<-as.numeric(Scores.predict.raw)
+  df$score<-as.integer(Scores.rounded)
+  colnames(df)<-c("CS_ID","raw_score","score")
+  return(df)
 }
 
-mainfunction.all('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'xxx', 'xxx', 'TEST', 5432)
+df<-mainfunction.all('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'xxx', 'xxxx', 'TEST', 5432)
+print(df)
