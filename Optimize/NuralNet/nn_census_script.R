f88dc3376157c8ecf598887d35004c657917acd0
Enrique O <a1saucegitrepository@gmail.com>
2021-05-08 01:35:16 -0700

    RMD and R script of census functions
    Both of these files are the same as the building. one rmd and one r script file. Currently problem is that the import.census isn't giving the dataframes we need. it outpues "" which is a problem (ꐦ°᷄д°᷅) ┌(▀Ĺ̯ ▀-͠ )┐

---
+++ Optimize/NuralNet/nn_census_script.R
@@ -0,0 +1,214 @@
+library(DBI)
+library(sqldf)
+library(dplyr)
+library(neuralnet)
+library(Metrics)
+library(caret)
+library(config)
+library(datapackage.r)
+library(jsonlite)
+library(fastDummies)
+library(datarium)
+library(ggplot2)
+
+
+get_data <- function(scores_or_new, server, user, password, database, port){
+  library(odbc)
+  con <- DBI::dbConnect(odbc::odbc(),
+                        driver = "PostgreSQL Unicode(x64)",
+                        database = as.character(database),
+                        UID      = as.character(user),
+                        PWD      = as.character(password),
+                        server = as.character(server),
+                        port = port)
+  import.census <- 'select
+     bg.bg_geo_id "Block Group ID"
+     ,(select avg(bgs.score) from "BG_Score" as bgs where bgs.bg_geo_id=bg.bg_geo_id) "Average Block Group Score"
+     ,max(case when dv.sid=\'pop\' then bgd.value Else 0 END) "Population"
+     ,max(case when dv.sid=\'pop_MF_3MS\' then bgd.value Else 0 END) "Population: 3 Mile"
+     ,max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) "Households: 3 Mile"
+    ,max(case when dv.sid=\'M_0_5\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_0_5\' then bgd.value Else 0 END) "Kids under 5"
+     ,round(cast((max(case when dv.sid=\'M_0_5\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_0_5\' then bgd.value Else 0 END)) /
+        max(case when dv.sid=\'pop\' then bgd.value Else null END) as numeric),3) "Percent Kids under 5"
+    ,max(case when dv.sid=\'M_0_5_3MS\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_0_5_3MS\' then bgd.value Else 0 END) "Kids under 5: 3 Mile"
+     ,round(cast((max(case when dv.sid=\'M_0_5_3MS\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_0_5_3MS\' then bgd.value Else 0 END)) /
+        max(case when dv.sid=\'pop_MF_3MS\' then bgd.value Else null END) as numeric),3) "Percent Kids under 5: 3 Mile"
+    ,max(case when dv.sid=\'M_5_9\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_5_9\' then bgd.value Else 0 END) "Kids 5 to 9"
+     ,round(cast((max(case when dv.sid=\'M_5_9\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_5_9\' then bgd.value Else 0 END)) /
+        max(case when dv.sid=\'pop\' then bgd.value Else null END) as numeric),3) "Percent Kids 5 to 9"
+    ,max(case when dv.sid=\'M_5_9_3MS\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_5_9_3MS\' then bgd.value Else 0 END) "Kids 5 to 9: 3 Mile"
+    ,round(cast((max(case when dv.sid=\'M_5_9_3MS\' then bgd.value Else 0 END)+max(case when dv.sid=\'F_5_9_3MS\' then bgd.value Else 0 END)) /
+         max(case when dv.sid=\'pop_MF_3MS\' then bgd.value Else null END) as numeric),3)  "Percent Kids 5 to 9: 3 Mile"
+    ,max(case when dv.sid=\'avg_age\' then bgd.value Else 0 END) "Average Age"
+    ,round(cast(sum(case when dv.sid in(\'hi_0_10_3MS\',\'hi_10_15_3MS\',\'hi_15_20_3MS\',\'hi_20_25_3MS\',\'hi_25_30_3MS\',\'hi_30_35_3MS\',\'hi_35_40_3MS\') then bgd.value  else 0 END) /
+      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income under 40K: 3 Mile"
+    ,round(cast(sum(case when dv.sid in(\'hi_40_45_3MS\',\'hi_45_50_3MS\') then bgd.value  else 0 END) /
+      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 40K to 50K: 3 Mile"
+    ,round(cast(sum(case when dv.sid in(\'hi_50_60_3MS\') then bgd.value  else 0 END) /
+      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 50K to 60K: 3 Mile"
+    ,round(cast(sum(case when dv.sid in(\'hi_60_75_3MS\') then bgd.value  else 0 END) /
+      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 60K to 75K: 3 Mile"
+    ,round(cast(sum(case when dv.sid in(\'hi_75_100_3MS\') then bgd.value  else 0 END) /
+      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 75K to 100K: 3 Mile"
+    ,round(cast(sum(case when dv.sid in(\'hi_100_125_3MS\') then bgd.value  else 0 END) /
+      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 100K to 125K: 3 Mile"
+    ,round(cast(sum(case when dv.sid in(\'hi_125_150_3MS\') then bgd.value  else 0 END) /
+      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 125K to 150K: 3 Mile"
+    ,round(cast(sum(case when dv.sid in(\'hi_150_200_3MS\') then bgd.value  else 0 END) /
+      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 150K to 200K: 3 Mile"
+    ,round(cast(sum(case when dv.sid in(\'hi_200_999_3MS\') then bgd.value  else 0 END) /
+      max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END) as numeric),3) "Household income 200K+: 3 Mile"
+  from "Block_Group" as bg
+  left join "BG_Data" as bgd on bg.bg_geo_id = bgd.bg_geo_id
+  inner join "Demo_Var" as dv on dv.full_variable_id=bgd.variable_id
+  group by bg.bg_geo_id
+  having
+  max(case when dv.sid=\'pop\' then bgd.value Else 0 END) > 0
+  and max(case when dv.sid=\'hi_tot_3MS\' then bgd.value Else 0 END)>0'
+  
+  census.import <- dbGetQuery(con,import.census)
+  
+  if(scores_or_new == 'scores'){
+    census.scores <- sqldf('SELECT "Block Group ID" AS "Block.Group.ID", "Average Block Group Score" AS "Average.Block.Group.Score", Population, 
+                           "Population: 3 Mile" AS "Population.3.Mile", "Households: 3 Mile" AS "Households.3.Mile", 
+                           "Percent Kids under 5" AS "Percent.Kids.under.5", "Percent Kids under 5: 3 Mile" AS "Percent.Kids.under.5.3.Mile", 
+                           "Percent Kids 5 to 9" AS "Percent.Kids.5.to.9", "Percent Kids 5 to 9: 3 Mile" AS "Percent.Kids.5.to.9.3 Mile", 
+                           "Average Age" AS "Average.Age", "Household income under 40K: 3 Mile" AS "Household.income.under.40K.3Mile", 
+                           "Household income 40K to 50K: 3 Mile" AS "Household.income.40K.to.50K.3.Mile", 
+                           "Household income 50K to 60K: 3 Mile" AS "Household.income.50K.to.60K.3.Mile", 
+                           "Household income 60K to 75K: 3 Mile" AS "Household.income.60K.to.75K.3.Mile", 
+                           "Household income 75K to 100K: 3 Mile" AS "Household.income.75K.to.100K.3.Mile", 
+                           "Household income 100K to 125K: 3 Mile" AS "Household.income.100:.125K.3.Mile", 
+                           "Household income 125K to 150K: 3 Mile" AS "Household.income.125K.to.150K.3.Mile", 
+                           "Household income 150K to 200K: 3 Mile" AS "Household.income.150K.to.200K.3.Mile", 
+                           "Household income 200K+: 3 Mile"  AS "Household.income.200K+.3Mile"FROM "census.import" 
+                           WHERE "Average Block Group Score" IS NOT NULL')
+  }
+  if(scores_or_new == 'new'){
+    census.new <- sqldf('SELECT "Block Group ID" AS "Block.Group.ID", "Average Block Group Score" AS "Average.Block.Group.Score", Population, 
+                           "Population: 3 Mile" AS "Population.3.Mile", "Households: 3 Mile" AS "Households.3.Mile", 
+                           "Percent Kids under 5" AS "Percent.Kids.under.5", "Percent Kids under 5: 3 Mile" AS "Percent.Kids.under.5.3.Mile", 
+                           "Percent Kids 5 to 9" AS "Percent.Kids.5.to.9", "Percent Kids 5 to 9: 3 Mile" AS "Percent.Kids.5.to.9.3 Mile", 
+                           "Average Age" AS "Average.Age", "Household income under 40K: 3 Mile" AS "Household.income.under.40K.3Mile", 
+                           "Household income 40K to 50K: 3 Mile" AS "Household.income.40K.to.50K.3.Mile", 
+                           "Household income 50K to 60K: 3 Mile" AS "Household.income.50K.to.60K.3.Mile", 
+                           "Household income 60K to 75K: 3 Mile" AS "Household.income.60K.to.75K.3.Mile", 
+                           "Household income 75K to 100K: 3 Mile" AS "Household.income.75K.to.100K.3.Mile", 
+                           "Household income 100K to 125K: 3 Mile" AS "Household.income.100:.125K.3.Mile", 
+                           "Household income 125K to 150K: 3 Mile" AS "Household.income.125K.to.150K.3.Mile", 
+                           "Household income 150K to 200K: 3 Mile" AS "Household.income.150K.to.200K.3.Mile", 
+                           "Household income 200K+: 3 Mile"  AS "Household.income.200K+.3Mile"FROM "census.import" 
+                           WHERE "Average Block Group Score" IS NULL')
+  }
+  if(scores_or_new == 'all'){
+    census.new <- sqldf('SELECT "Block Group ID" AS "Block.Group.ID", "Average Block Group Score" AS "Average.Block.Group.Score", Population, 
+                           "Population: 3 Mile" AS "Population.3.Mile", "Households: 3 Mile" AS "Households.3.Mile", 
+                           "Percent Kids under 5" AS "Percent.Kids.under.5", "Percent Kids under 5: 3 Mile" AS "Percent.Kids.under.5.3.Mile", 
+                           "Percent Kids 5 to 9" AS "Percent.Kids.5.to.9", "Percent Kids 5 to 9: 3 Mile" AS "Percent.Kids.5.to.9.3 Mile", 
+                           "Average Age" AS "Average.Age", "Household income under 40K: 3 Mile" AS "Household.income.under.40K.3Mile", 
+                           "Household income 40K to 50K: 3 Mile" AS "Household.income.40K.to.50K.3.Mile", 
+                           "Household income 50K to 60K: 3 Mile" AS "Household.income.50K.to.60K.3.Mile", 
+                           "Household income 60K to 75K: 3 Mile" AS "Household.income.60K.to.75K.3.Mile", 
+                           "Household income 75K to 100K: 3 Mile" AS "Household.income.75K.to.100K.3.Mile", 
+                           "Household income 100K to 125K: 3 Mile" AS "Household.income.100:.125K.3.Mile", 
+                           "Household income 125K to 150K: 3 Mile" AS "Household.income.125K.to.150K.3.Mile", 
+                           "Household income 150K to 200K: 3 Mile" AS "Household.income.150K.to.200K.3.Mile", 
+                           "Household income 200K+: 3 Mile"  AS "Household.income.200K+.3Mile"FROM "census.import"')
+  }
+  return("")
+}
+
+
+clean_census_data <- function(dataframe){
+  census.scores <- data.frame
+  
+  min.max.norm <- function(x) {
+    (x - min(x)) / (max(x) - min(x))
+  }
+  
+  for (i in 2:length(census.scores)) {
+    census.scores[i] <- min.max.norm(census.scores[i])
+  }
+}
+
+nn.model.score <- function(data){
+  #insert model
+  set.seed(5)
+  neun.cens <- neuralnet(Average.Block.Group.Score ~ Population + Population.3.Mile + Households.3.Mile + Percent.Kids.under.5 + 
+                                  Percent.Kids.under.5.3.Mile + Percent.Kids.5.to.9 + Average.Age + Household.income.40K.to.50K.3.Mile + 
+                                  Household.income.50K.to.60K.3.Mile + Household.income.60K.to.75K.3.Mile, data = cens.train, linear.output = T, 
+                                  hidden = c(4,1))
+  #extract scores and set scale 1-5
+  predicted.scores <- prediction(neun.cens)
+  predict.scores <- predicted.scores$rep1[,11]
+  norm.predict.scores <- predict.scores * 5
+  Scores.predict <- data.frame(data$CS_ID, norm.predict.scores)
+  return(Scores.predict)
+}
+
+nn.model.new <- function(server, user, password, database, port, data){
+  got_scores_data <- get_data("scores", as.character(server), as.character(user), as.character(password), as.character(database), port)
+  cleaned.scores.data <- clean_census_data(got_scores_data)
+  #insert model
+  set.seed(3)
+  neun.cens <- neuralnet(Average.Block.Group.Score ~ Population + Population.3.Mile + Households.3.Mile + Percent.Kids.under.5 + 
+                           Percent.Kids.under.5.3.Mile + Percent.Kids.5.to.9 + Average.Age + Household.income.40K.to.50K.3.Mile + 
+                           Household.income.50K.to.60K.3.Mile + Household.income.60K.to.75K.3.Mile, data = cens.train, linear.output = T, 
+                           hidden = c(4,1))
+  return(neun.cens)
+}
+
+new.predictions <- function(data1, data2){
+  Scores.predict.new <- predict(data1, data2, all.units = FALSE)
+  return(Scores.predict.new)
+}
+
+mainfunction.scores <- function(server, user, password, database, port){
+  
+  got_scores_data <- get_data("scores", as.character(server), as.character(user), as.character(password), as.character(database), port)
+  
+  clean.scores.data <- clean_census_data(got_scores_data)
+  
+  score.model <- nn.model(clean.scores.data)
+  
+  return(score.model)
+}
+
+mainfunction.scores('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'eotanez', 's4I8$cPPRX5x', 'TEST', 5432)
+
+mainfunction.new <- function(server, user, password, database, port){
+  #get the data
+  
+  got_new_data <- get_data("new", as.character(server), as.character(user), as.character(password), as.character(database), port)
+  #clean the data
+  
+  clean.new.data <- clean_census_data(got_new_data)
+  #load in the subfunction
+  
+  model.new <- nn.model.new('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'eotanez', 's4I8$cPPRX5x', 'TEST', 5432)
+  
+  #then predict with new
+  
+  Scores.predict.new <- new.predictions(model.new,clean.new.data) * 5
+  return(Scores.predict.new)
+}
+
+mainfunction.new('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'eotanez', 's4I8$cPPRX5x', 'TEST', 5432)
+
+mainfunction.all <- function(server, user, password, database, port){
+  #get the data
+  
+  got_all_data <- get_data("all", as.character(server), as.character(user), as.character(password), as.character(database), port)
+  #clean the data
+  
+  clean.all.data <- clean_census_data(got_all_data)
+  #load in the subfunction
+  
+  model.all <- nn.model.new('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'eotanez', 's4I8$cPPRX5x', 'TEST', 5432)
+  #predict with scores first
+  
+  Scores.predict.all <- new.predictions(model.all, clean.all.data) * 5
+  return(Scores.predict.all)
+}
+
+mainfunction.all('greentrike.cfvgdrxonjze.us-west-2.rds.amazonaws.com', 'eotanez', 's4I8$cPPRX5x', 'TEST', 5432)
