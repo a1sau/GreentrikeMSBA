4225721b9b1b3ac0b6b3672e2d38a61e2a4dcdc6
Kobol-Coder <lugalot82@hotmail.com>
2021-05-19 19:07:47 -0700

    Fix double count in household income

--- census_dev/score_file_generator.py
+++ census_dev/score_file_generator.py
@@ -122,25 +122,26 @@ def select_census(conn,user='',limit=10):
         max(case when dv.sid='pop' then bgd.value Else null END) as numeric),3) "%Percent Kids 5 to 9"
     ,round(cast((max(case when dv.sid='M_5_9_3MS' then bgd.value Else 0 END)+max(case when dv.sid='F_5_9_3MS' then bgd.value Else 0 END)) /
          max(case when dv.sid='pop_MF_3MS' then bgd.value Else null END) as numeric),3)  "%Percent Kids 5 to 9: 3 Miles"
+    ,max(case when dv.sid='armf_3MS' then bgd.value else 0 END) "Armed Forces: 3 Mile"
     ,max(case when dv.sid='avg_age' then bgd.value Else 0 END) "Average Age"
     ,round(cast(sum(case when dv.sid in('hi_0_10_3MS','hi_10_15_3MS','hi_15_20_3MS','hi_20_25_3MS','hi_25_30_3MS','hi_30_35_3MS','hi_35_40_3MS') then bgd.value  else 0 END) /
-      max(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income under 40K: 3 Mile"
+      sum(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income under 40K: 3 Mile"
     ,round(cast(sum(case when dv.sid in('hi_40_45_3MS','hi_45_50_3MS') then bgd.value  else 0 END) /
-      max(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 40K to 50K: 3 Mile"
+      sum(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 40K to 50K: 3 Mile"
     ,round(cast(sum(case when dv.sid in('hi_50_60_3MS') then bgd.value  else 0 END) /
-      max(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 50K to 60K: 3 Mile"
+      sum(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 50K to 60K: 3 Mile"
     ,round(cast(sum(case when dv.sid in('hi_60_75_3MS') then bgd.value  else 0 END) /
-      max(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 60K to 75K: 3 Mile"
+      sum(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 60K to 75K: 3 Mile"
     ,round(cast(sum(case when dv.sid in('hi_75_100_3MS') then bgd.value  else 0 END) /
-      max(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 75K to 100K: 3 Mile"
+      sum(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 75K to 100K: 3 Mile"
     ,round(cast(sum(case when dv.sid in('hi_100_125_3MS') then bgd.value  else 0 END) /
-      max(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 100K to 125K: 3 Mile"
+      sum(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 100K to 125K: 3 Mile"
     ,round(cast(sum(case when dv.sid in('hi_125_150_3MS') then bgd.value  else 0 END) /
-      max(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 125K to 150K: 3 Mile"
+      sum(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 125K to 150K: 3 Mile"
     ,round(cast(sum(case when dv.sid in('hi_150_200_3MS') then bgd.value  else 0 END) /
-      max(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 150K to 200K: 3 Mile"
+      sum(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 150K to 200K: 3 Mile"
     ,round(cast(sum(case when dv.sid in('hi_200_999_3MS') then bgd.value  else 0 END) /
-      max(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 200K+: 3 Mile"
+      sum(case when dv.sid='hi_tot_3MS' then bgd.value Else 0 END) as numeric),3) "%Household income 200K+: 3 Mile"
     ,'' as "Block Group Score"
     from "Block_Group" as bg
     left join "BG_Data" as bgd on bg.bg_geo_id = bgd.bg_geo_id
@@ -157,6 +158,7 @@ def select_census(conn,user='',limit=10):
     """.format(user_filter,limit)
     # cur = conn.cursor()
     # cur.execute(sql_command)
+    print(sql_command)
     try:
         df_var=pd.read_sql_query(sql_command,conn)
     except (Exception, psycopg2.DatabaseError) as err:
